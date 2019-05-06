10dcaa5c1d5fc3296c2b2d1df3d04b56fed1ec76
Poramet003 <Poramet003@users.noreply.github.com>
2019-05-07 01:05:15 +0700

    update java and layout file

--- app/src/main/java/com/example/appclinic/Activities/ExaminationActivity.java
+++ app/src/main/java/com/example/appclinic/Activities/ExaminationActivity.java
@@ -1,10 +1,117 @@
 package com.example.appclinic.Activities;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.appclinic.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
-public class ExaminationActivity {
+public class ExaminationActivity extends AppCompatActivity {
+
+    private EditText mSearchField;
+    private ImageButton mSearchBtn;
+
+    private RecyclerView mResultList;
+
+    private DatabaseReference mUserDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        mUserDatabase = FirebaseDatabase.getInstance().getReference("patient");
 
 
-}
+        mSearchField = (EditText) findViewById(R.id.search_idcard);
+        mSearchBtn = (ImageButton) findViewById(R.id.search_btn);
+
+        mResultList = (RecyclerView) findViewById(R.id.result_list);
+        mResultList.setHasFixedSize(true);
+        mResultList.setLayoutManager(new LinearLayoutManager(this));
+
+        mSearchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String searchText = mSearchField.getText().toString();
+
+                firebaseUserSearch(searchText);
+
+            }
+        });
+
+    }
+
+    private void firebaseUserSearch(String searchText) {
+
+        Toast.makeText(ExaminationActivity.this, "Started Search", Toast.LENGTH_LONG).show();
+
+        Query firebaseSearchQuery = mUserDatabase.orderByChild("name").startAt(searchText).endAt(searchText + "\uf8ff");
+
+        FirebaseRecyclerAdapter<Patient, UsersViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<UsersViewHolder, Patient>(
+
+                Patient.class,
+                R.layout.list_layout,
+                UsersViewHolder.class,
+                firebaseSearchQuery
+
+        ) {
+            @Override
+            protected void populateViewHolder(UsersViewHolder viewHolder, Patient model, int position) {
+
+
+                viewHolder.setDetails(getApplicationContext(), model.getPatientFname(), model.get(), model.getImage());
+
+            }
+        };
+
+        mResultList.setAdapter(firebaseRecyclerAdapter);
+
+    }
+
+
+    // View Holder Class
+
+    public static class UsersViewHolder extends RecyclerView.ViewHolder {
+
+        View mView;
+
+
+        public UsersViewHolder(View itemView) {
+            super(itemView);
+
+            mView = itemView;
+
+        }
+
+        public void setDetails(Context ctx, String patientCard, String patientFname, String patientLname ){
+
+            TextView p_Card = (TextView) mView.findViewById(R.id.regCard);
+            TextView p_Fname = (TextView) mView.findViewById(R.id.regFname);
+            TextView p_Lname = (TextView) mView.findViewById(R.id.regLname);
+//            ImageView p_image = (ImageView) mView.findViewById(R.id.regPatientPhoto);
+
+
+            p_Fname.setText(patientFname);
+            p_Lname.setText(patientLname);
+            p_Card.setText(patientCard);
+//            Glide.with(ctx).load(userImage).into(user_image);
+
+        }
+
+
+    }
+
